d53e2ca3002b5bd35fe12aaeedab4517fe278467
dikshabhange30 <dikshabhange3025@gmail.com>
2026-08-18 16:53:47 +0530

    Add JWT authentication foundation

---
+++ app/oauth2.py
@@ -0,0 +1,8 @@
+#  JWT/token verification
+
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
---
+++ app/routers/auth.py
@@ -0,0 +1,5 @@
+# register/login API endpoints
+# "What URL should the frontend call for login/register?"
+# pip install passlib[bcrypt] python-jose[cryptography]
+# passlib → password hashing
+# python-jose → creating and verifying JWT tokens
